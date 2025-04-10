f04f01fe6878fef6aeacb6f99686c9c3944f1896
hoangtd <hoangtd@mor.com.vn>
2025-04-10 10:53:36 +0700

    commitday10/4

--- demo-item3.py
+++ demo-item3.py
@@ -1,31 +1,46 @@
 import json
 import os
 
-# Kiểm tra xem file đã có data bên trong chưa
-users = []
 
-if os.path.exists('users.json') and os.path.getsize('users.json') > 0:
-    with open('users.json', 'r') as f:
-        users = json.load(f)
+def read_user_from_file(filename):
+    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    return []
 
-# Nhập người dùng mới
-while True:
-    name = input("Nhập tên: ")
-    age = int(input("Nhập tuổi: "))
-    skills = input("Nhập kỹ năng (phân cách bởi dấu phẩy): ").split(',')
+
+def get_user_input():
+    name = input('Vui lòng nhập tên: ')
+    age = int(input('Vui lòng nhập tuổi: '))
+    skills = input("Nhập kĩ năng (phân tách bằng dấu phẩy): ")
 
     user = {
-        "name": name,
-        "age": age,
-        "skills": [skill.strip() for skill in skills]
+        'name': name,
+        'age': age,
+        'skill': [skill.strip() for skill in skills.split(',')]
     }
- # Thêm vào danh sách đã có
-    users.append(user) 
+    return user
 
-    tiep = input("Nhập thêm người nữa? (y/n): ")
-    if tiep.lower() != 'y':
-        break
 
-#Ghi toàn bộ danh sách người dùng vào file 
-with open('users.json', 'w') as f:
-    json.dump(users, f, indent=4)
+def save_user_to_file(filename, users):
+    with open(filename, 'w') as f:
+        json.dump(users, f, indent=4)
+
+
+def main():
+    filename = 'users.json'
+    users = read_user_from_file(filename)
+
+    while True:
+        user = get_user_input()
+        users.append(user)
+
+        cont = input('Bạn có muốn nhập tiếp không (y/n)? ')
+        if cont.lower() != 'y':
+            break
+
+    save_user_to_file(filename, users)
+
+
+if __name__ == '__main__':
+    main()
